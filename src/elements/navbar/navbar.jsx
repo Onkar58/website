81bbc86833d067cd00b5755de64f8c2a6fe24f35
Onkar58 <onkarwaghmode58@gmail.com>
2023-01-23 17:20:35 +0530

    Made a new Navbar that will work for the mobile and tablet devices

--- src/elements/navbar/navbar.jsx
+++ src/elements/navbar/navbar.jsx
@@ -2,29 +2,29 @@ import React from 'react'
 import styles from './navbar.module.css'
 // import {NavLink} from 'react-router-dom'
 import navbarInfo from '../info/navbarInfo'
+import { useState } from 'react'
+
+
+
 
 function Navbar({ url }) {
-  return (
-    <>
-      <nav className={styles.navbar}>
+
+return (
+  <>
+    {/* <nav className={styles.navbar}>
         <ul>
           {navbarInfo.map((currentValue) => <li><a href={currentValue.url}>{currentValue.title}</a></li>)}
           </ul>
-        </nav>
-      {/* <nav className={styles.navbar2}>
-        <h3>HOME</h3> 
-        <ul>
-          <li><a href='#'>Home</a></li>
-          <li><a href="#">Menu ^</a>
-            <ul className={styles.dropdown}>
-              {navbarInfo.map((currentValue) => <li><a href={currentValue.url}>{currentValue.title}</a></li>)}
-            </ul>
-          </li>
-          <li><a href='#'>About</a></li>
-        </ul>
-      </nav> */}
-    </>
-  )
+  </nav>*/}
+    <nav className={styles.navbar2}>
+      <h3>HOME</h3>
+      <button><i className="fa-solid fa-angle-down"></i></button>
+      <ul id="list1">
+        {navbarInfo.map((currentValue) => <li><a href={currentValue.url}>{currentValue.title}</a></li>)}
+      </ul>
+    </nav>
+  </>
+)
 }
 
 export default Navbar
