46adde3d95338a3978ff73b9d5f4d7818440aec9
Onkar58 <onkarwaghmode58@gmail.com>
2023-01-23 19:20:18 +0530

    made the navbar responsive but links and current page remains

--- src/elements/navbar/navbar.jsx
+++ src/elements/navbar/navbar.jsx
@@ -10,24 +10,37 @@ import { useState } from 'react'
 function Navbar({ url }) {
 
   const [btnState, setBtnState] = useState(false);
-
+  const [mystyle, setMyStyle] = useState({
+    display: 'none'
+  })
   function click1() {
     setBtnState(btnState => !btnState)
+    if (mystyle.display == 'none') {
+      setMyStyle({
+        display: 'flex',
+      })
+    }
+    else {
+      setMyStyle({
+        display: 'none',
+      })
+    }
   }
 
-  let toggleClassState = btnState ? ' fa-angle-up' : ' fa-angle-down';
+  let arrowToggle = btnState ? ' fa-angle-up' : ' fa-angle-down';
+  let navDispay = btnState ? 'display: flex;' : 'display: none;';
 
   return (
     <>
-      {/* <nav className={styles.navbar}>
+      <nav className={styles.navbar}>
         <ul>
           {navbarInfo.map((currentValue) => <li><a href={currentValue.url}>{currentValue.title}</a></li>)}
-          </ul>
-  </nav>*/}
+        </ul>
+      </nav>
       <nav className={styles.navbar2}>
         <h3>HOME</h3>
-        <button className='' onClick={click1}><i className={`fa-solid${toggleClassState}`}></i></button>
-        <ul id="list1">
+        <button className='' onClick={click1}><i className={`fa-solid${arrowToggle}`}></i></button>
+        <ul id="list1" style={mystyle}>
           {navbarInfo.map((currentValue) => <li><a href={currentValue.url}>{currentValue.title}</a></li>)}
         </ul>
       </nav>
